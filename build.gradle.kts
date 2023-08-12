8304e33f950a24a688b54190686ad647ceb36858
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-12 13:18:42 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "studio.hcmc"
-version = "0.0.11"
+version = "0.0.12"
 
 repositories {
     mavenCentral()
@@ -21,7 +21,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "studio.hcmc"
             artifactId = "spring-protocol-extension"
-            version = "0.0.11"
+            version = "0.0.12"
             from(components["java"])
         }
     }
